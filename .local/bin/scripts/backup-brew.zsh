3f9e7860fb9a0033f32a5529d978e8680059cb4b
jmt1423 <60321773+jmt1423@users.noreply.github.com>
2025-05-24 07:46:26 +0100

    updated brew script and packages

--- .local/bin/scripts/backup-brew.zsh
+++ .local/bin/scripts/backup-brew.zsh
@@ -5,4 +5,4 @@ DATE=`date +%d-%m-%Y`
 cd /Users/jont/.brew-backups/tmp/
 brew bundle dump
 mv Brewfile "/Users/jont/.brew-backups/Brewfile-$DATE"
-chezmoi add "/Users/jont/.brew-backups/Brewfile-$DATE"
+yadm add "/Users/jont/.brew-backups/Brewfile-$DATE"
